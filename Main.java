ef87dda4d9fc4d6085f5763676282558ebf16bea
spish-web <nickolas.spisak@gmail.com>
2022-07-14 00:42:21 -0400

    array list

--- Main.java
+++ Main.java
@@ -1,13 +1,26 @@
-import javax.swing.JOptionPane;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-    
-          String name = JOptionPane.showInputDialog("Enter your name");
-          JOptionPane.showMessageDialog(null, "Hello"+name);
-            int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-            JOptionPane.showMessageDialog(null, "You are"+age);
+      ArrayList<ArrayList<String>> groceryList = new ArrayList();
+      
+      ArrayList<String> bakeryList = new ArrayList<String>();
+      bakeryList.add("pasta");
+      bakeryList.add("garlic bread");
+      bakeryList.add("donuts");
+      ArrayList<String> produceList = new ArrayList<String>();
+      produceList.add("tomatoes");
+      produceList.add("zucchini");
+      produceList.add("peppers");
+      ArrayList<String> drinkList = new ArrayList<String>();
+      drinkList.add("soda");
+      drinkList.add("coffee");
 
+      groceryList.add(bakeryList);
+      groceryList.add(produceList);
+      groceryList.add(drinkList);
+      System.out.println(groceryList);
+      
         }
     
 }
