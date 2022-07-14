6c0a4a948e0077bf1737084a293c5f2a22f151a7
spish-web <nickolas.spisak@gmail.com>
2022-07-14 04:01:38 -0400

    interface test

--- Main.java
+++ Main.java
@@ -1,26 +1,11 @@
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-      ArrayList<ArrayList<String>> groceryList = new ArrayList();
-      
-      ArrayList<String> bakeryList = new ArrayList<String>();
-      bakeryList.add("pasta");
-      bakeryList.add("garlic bread");
-      bakeryList.add("donuts");
-      ArrayList<String> produceList = new ArrayList<String>();
-      produceList.add("tomatoes");
-      produceList.add("zucchini");
-      produceList.add("peppers");
-      ArrayList<String> drinkList = new ArrayList<String>();
-      drinkList.add("soda");
-      drinkList.add("coffee");
+      Hawk hawk = new Hawk();
+      hawk.hunt();
 
-      groceryList.add(bakeryList);
-      groceryList.add(produceList);
-      groceryList.add(drinkList);
-      System.out.println(groceryList);
-      
-        }
+      Rabbit rabbit = new Rabbit();
+      rabbit.flee();
+    }
     
 }
